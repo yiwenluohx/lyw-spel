ca6f6c89761ddbaca017063c981e61fe486c438c
luohx <Lhongxiao@lcchain.com>
2023-08-15 09:54:27 +0800

    创建spel项目

---
+++ src/main/java/com/study/lywspel/LywSpelApplication.java
@@ -0,0 +1,16 @@
+package com.study.lywspel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luohongxiao
+ */
+@SpringBootApplication
+public class LywSpelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LywSpelApplication.class, args);
+    }
+
+}
